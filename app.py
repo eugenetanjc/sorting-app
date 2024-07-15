b74d511231949b14ccb0a95cedcd9f9f05c2c2f7
Eugene Tan <68592426+eugenetanjc@users.noreply.github.com>
2024-07-15 09:40:55 +0000

    Set password

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import libraries
 import streamlit as st
+import hmac
 import pandas as pd
 import os
 import io
@@ -155,6 +156,32 @@ def sorting_layout():
     st.markdown('<hr>', unsafe_allow_html=True)
 
     st.header("Sorting")
+
+    def check_password():
+        """Returns `True` if the user had the correct password."""
+
+        def password_entered():
+            """Checks whether a password entered by the user is correct."""
+            if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+                st.session_state["password_correct"] = True
+                del st.session_state["password"]  # Don't store the password.
+            else:
+                st.session_state["password_correct"] = False
+
+        # Return True if the password is validated.
+        if st.session_state.get("password_correct", False):
+            return True
+
+        # Show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        if "password_correct" in st.session_state:
+            st.error("😕 Password incorrect")
+        return False
+
+    if not check_password():
+        st.stop()  # Do not continue if check_password is not True.
 
     # Upload Parameters File
     st.subheader("Upload Parameters File")
